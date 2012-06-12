1345b890402446c4054b1c3b462af2841108a474
Matthew Lewis <matt@mplewis.com>
2012-06-11 21:32:26 -0500

    Added file with piano frequencies

---
+++ pianoFreqs.c
@@ -0,0 +1,100 @@
+/* frequencies of piano keys for arduino/C programs
+ * taken from https://en.wikipedia.org/wiki/Piano_key_frequencies
+ */
+
+const float nC8  = 4186.01;
+
+const float nB7  = 3951.07;
+const float nAs7 = 3729.31;
+const float nA7  = 3520.00;
+const float nGs7 = 3322.44;
+const float nG7  = 3135.96;
+const float nFs7 = 2959.96;
+const float nF7  = 2793.83;
+const float nE7  = 2637.02;
+const float nDs7 = 2489.02;
+const float nD7  = 2349.32;
+const float nCs7 = 2217.46;
+const float nC7  = 2093.00;
+
+const float nB6  = 1975.53;
+const float nAs6 = 1864.66;
+const float nA6  = 1760.00;
+const float nGs6 = 1661.22;
+const float nG6  = 1567.98;
+const float nFs6 = 1479.98;
+const float nF6  = 1396.91;
+const float nE6  = 1318.51;
+const float nDs6 = 1244.51;
+const float nD6  = 1174.66;
+const float nCs6 = 1108.73;
+const float nC6  = 1046.50;
+
+const float nB5  = 987.767;
+const float nAs5 = 932.328;
+const float nA5  = 880.000;
+const float nGs5 = 830.609;
+const float nG5  = 783.991;
+const float nFs5 = 739.989;
+const float nF5  = 698.456;
+const float nE5  = 659.255;
+const float nDs5 = 622.254;
+const float nD5  = 587.330;
+const float nCs5 = 554.365;
+const float nC5  = 523.251;
+
+const float nB4  = 493.883;
+const float nAs4 = 466.164;
+const float nA4  = 440.000;
+const float nGs4 = 415.305;
+const float nG4  = 391.995;
+const float nFs4 = 369.994;
+const float nF4  = 349.228;
+const float nE4  = 329.628;
+const float nDs4 = 311.127;
+const float nD4  = 293.665;
+const float nCs4 = 277.183;
+const float nC4  = 261.626;
+
+const float nB3  = 246.942;
+const float nAs3 = 233.082;
+const float nA3  = 220.000;
+const float nGs3 = 207.652;
+const float nG3  = 195.998;
+const float nFs3 = 184.997;
+const float nF3  = 174.614;
+const float nE3  = 164.814;
+const float nDs3 = 155.563;
+const float nD3  = 146.832;
+const float nCs3 = 138.591;
+const float nC3  = 130.813;
+
+const float nB2  = 123.471;
+const float nAs2 = 116.541;
+const float nA2  = 110.000;
+const float nGs2 = 103.826;
+const float nG2  = 97.9989;
+const float nFs2 = 92.4986;
+const float nF2  = 87.3071;
+const float nE2  = 82.4069;
+const float nDs2 = 77.7817;
+const float nD2  = 73.4162;
+const float nCs2 = 69.2957;
+const float nC2  = 65.4064;
+
+const float nB1  = 61.7354;
+const float nAs1 = 58.2705;
+const float nA1  = 55.0000;
+const float nGs1 = 51.9131;
+const float nG1  = 48.9994;
+const float nFs1 = 46.2493;
+const float nF1  = 43.6535;
+const float nE1  = 41.2034;
+const float nDs1 = 38.8909;
+const float nD1  = 36.7081;
+const float nCs1 = 34.6478;
+const float nC1  = 32.7032;
+
+const float nB0  = 30.8677;
+const float nAs0 = 29.1352;
+const float nA0  = 27.5000;
